34f6aa226a69b2d25b69444793f488132bdd49f1
mankaistep <hhoang21vn@gmail.com>
2021-08-04 10:54:01 +0700

    Fix bugs

--- src/main/java/mk/plugin/lockeditems/listener/LIListener.java
+++ src/main/java/mk/plugin/lockeditems/listener/LIListener.java
@@ -17,6 +17,7 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.*;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.LlamaInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
@@ -77,6 +78,9 @@ public class LIListener implements Listener {
 			}
 
 		}
+		else {
+			if (LIUtils.isLocked(i)) e.setCancelled(true);
+		}
 	}
 
 	@EventHandler
@@ -130,6 +134,10 @@ public class LIListener implements Listener {
 
 	@EventHandler
 	public void onClick(InventoryClickEvent e) {
+		if (e.getInventory() instanceof LlamaInventory) {
+			e.setCancelled(true);
+			return;
+		}
 		ClickType clicktype = e.getClick();
 		if (clicktype.equals(ClickType.NUMBER_KEY)) {
 			e.setCancelled(true);
